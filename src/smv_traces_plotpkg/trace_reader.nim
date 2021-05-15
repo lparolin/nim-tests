d30247dae57c1c563223e8672a517c6daf3b01b7
Luca Parolini <mail@lucaparolini.com>
2021-05-15 21:01:48 +0200

    First version with flexible table entry now working

--- src/smv_traces_plotpkg/trace_reader.nim
+++ src/smv_traces_plotpkg/trace_reader.nim
@@ -2,6 +2,61 @@ import streams
 import strutils
 import nre
 import system
+import tables 
+import hashes
+
+type EntryType* = enum 
+    nkInt, 
+    nkString,
+    nkBool
+
+type GenericEntry = object 
+  case kind*: EntryType
+  of nkInt: 
+    intVal: int
+  of nkString:
+    stringVal: string
+  of nkBool:
+    boolVal: bool 
+
+proc hash*(entry: GenericEntry): Hash = 
+  case entry.kind:
+    of nkInt:
+      return hash(entry.intVal)
+    of nkString:
+      return hash(entry.stringVal)
+    of nkBool:
+      return hash(entry.boolVal)
+
+proc `==`*(a: GenericEntry, b: GenericEntry): bool = 
+  if a.kind != b.kind: 
+    return false 
+  case a.kind:
+    of nkInt:
+      return a.intVal == b.intVal
+    of nkString:
+      return a.stringVal == b.stringVal
+    of nkBool:
+      return a.boolVal == b.boolVal
+
+proc getValue*[T](a: GenericEntry): T = 
+  case a.kind:
+    of nkInt:
+      return a.intVal
+    of nkString:
+      return a.stringVal
+    of nkBool:
+      return a.boolVal
+
+proc makeEntry*(value: int) : GenericEntry =
+  return GenericEntry(kind: nkInt, intVal: value) 
+
+proc makeEntry*(value: string) : GenericEntry =
+  return GenericEntry(kind: nkString, stringVal: value) 
+
+proc makeEntry*(value: bool) : GenericEntry =
+  return GenericEntry(kind: nkBool, boolVal: value) 
+
 
 const iteration_line_matcher_string = "\\s*-> State:\\s+(\\d+)\\.(\\d+)\\s<-"
 
@@ -27,3 +82,7 @@ func isIterationLine*(instring: string) : bool =
 proc getNumberOfSteps*(instream : Stream) : int = 
   var line : string 
   return 2
+
+
+proc getDataChunk*(trace_index: int, step_index: int, instream: string) : Table[string, GenericEntry] = 
+  return {"a": GenericEntry(kind: nkInt, intVal: 0)}.toTable
